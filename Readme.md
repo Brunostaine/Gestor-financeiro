5b62b3a56635ef70a439f81ef2165b0d3fe5f7bb
Bruno Staine <brunostaine@hotmail.com>
2023-12-22 16:09:39 -0300

    Recurso de auditoria, datas de criação e atualização

--- Readme.md
+++ Readme.md
@@ -30,6 +30,7 @@ Utilize ferramentas como postman ou insominia para testar os endponts
 - [x] Implementar Requisitos de autenticação
 - [x] Implementar Requisitos de metas
 - [ ] Implementar Requisitos de Receitas e despesas
+- [x] Implementar Auditoria
 - [ ] Implementar documentação com Swagger
 - [ ] Implementar testes de ponta a ponta
 
--- src/main/java/com/brunostaine/api/gestor/financeiro/entities/Usuario.java
+++ src/main/java/com/brunostaine/api/gestor/financeiro/entities/Usuario.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
